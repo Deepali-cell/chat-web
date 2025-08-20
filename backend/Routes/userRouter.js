7c77a4284a864ec8093faac6e8d7de8097583d0c
Deepali <joysingal247@gmail.com>
2025-08-20 21:21:57 +0530

    change

--- backend/Routes/userRouter.js
+++ backend/Routes/userRouter.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { allUsers, getProfile, loginUser, registerUser } from "../controller/usercontroller.js";
+import {
+  allUsers,
+  getProfile,
+  loginUser,
+  registerUser,
+} from "../controller/userController.js";
 import { authUser } from "../middleware/authUser.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/register" , registerUser );
-userRouter.post("/login" , loginUser );
-userRouter.get("/allusers" , authUser , allUsers );
-userRouter.get("/getprofile" , authUser , getProfile);
-
-
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/allusers", authUser, allUsers);
+userRouter.get("/getprofile", authUser, getProfile);
 
 export default userRouter;
